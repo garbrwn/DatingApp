dc507e5e3c6ffb031e6eab068d9caae459a0a6a3
garbrwn <garybrwn88@googlemail.com>
2021-12-13 10:48:45 +0000

    End of Section 8

---
+++ API/DTOs/PhotoDTO.cs
@@ -0,0 +1,9 @@
+namespace API.DTOs
+{
+    public class PhotoDTO
+    {
+        public int id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
